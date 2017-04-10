ae468ed49848e2a19b0970e7ada9eb35df86e333
Sebastian Holzapfel <seb.holzapfel@data61.csiro.au>
2017-04-10 10:35:33 +1000

    acamar MPU test is now more comprehensive

--- packages/machine-ek-tm4c123gxl/example/acamar-mpu-test.c
+++ packages/machine-ek-tm4c123gxl/example/acamar-mpu-test.c
@@ -37,14 +37,12 @@ void fn_a(void);
 void fn_b(void);
 void fatal(RtosErrorId error_id);
 
-/* domX_variable_2 is unused in this example, but remains to
- * illustrate usage of multiple symbols per domain */
+uint32_t dom1_variable = 0;
+uint32_t *dom1_stack_variable_addr = NULL;
 
-uint32_t dom1_variable_1 = 0;
-uint32_t dom1_variable_2 = 0;
+uint32_t dom2_variable = 0;
 
-uint32_t dom2_variable_1 = 0;
-uint32_t dom2_variable_2 = 0;
+uint32_t un_owned_global = 0;
 
 void
 fatal(const RtosErrorId error_id)
@@ -76,29 +74,55 @@ void iteration() {
     /* One of these increments should fault depending on
      * which task is currently executing */
 
-    debug_println("Increment dom1_variable_1...");
-    ++dom1_variable_1;
+    debug_println("increment dom1_variable...");
+    ++dom1_variable;
 
-    debug_println("Increment dom2_variable_1...");
-    ++dom2_variable_1;
+    debug_println("increment dom2_variable...");
+    ++dom2_variable;
 
     /* Both of these reads should succeed, both tasks have read access */
 
-    debug_print("dom1_variable_1=");
-    debug_printhex32(dom1_variable_1);
+    debug_print("dom1_variable=");
+    debug_printhex32(dom1_variable);
     debug_println("");
 
-    debug_print("dom2_variable_1=");
-    debug_printhex32(dom2_variable_1);
+    debug_print("dom2_variable=");
+    debug_printhex32(dom2_variable);
     debug_println("");
+
+    uint32_t x = 0;
+
+    debug_println("increment x (on my stack)...");
+    ++x;
+
+    /* Next 2 tests should fault in task 1, but not task 0 */
+
+    debug_println("increment variable on stack of task 0...");
+    ++(*dom1_stack_variable_addr);
+
+    debug_print("*dom1_stack_variable_addr=");
+    debug_printhex32(*dom1_stack_variable_addr);
+    debug_println("");
+
+    /* These should fault */
+
+    debug_println("access un_owned_global...");
+    x = un_owned_global;
+
+    debug_println("access kernel data...");
+    x = rtos_internal_current_task;
+
 }
 
 void
 fn_a(void)
 {
+    /* store the address of a variable on stack of task 0 */
+    uint32_t x = 0;
+    dom1_stack_variable_addr = &x;
 
-    debug_print("current task (KERNEL DATA): ");
-    debug_printhex32(rtos_internal_current_task); /* should fault */
+    debug_print("dom1_stack_variable_addr=");
+    debug_printhex32((uint32_t)dom1_stack_variable_addr);
     debug_println("");
 
     for (;;)
